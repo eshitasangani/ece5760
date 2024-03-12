16b82285e3597822186c19d7fe7570c58874b545
Katherine Zhou <kz273@cornell.edu>
2024-03-11 21:01:07 -0400

    col of nodes not working

--- lab3/drum_syn_tb.v
+++ lab3/drum_syn_tb.v
@@ -26,59 +26,228 @@ initial begin
     reset  = 1'b0;
 end
 
-wire [17:0] u_next [29:0];
-reg  [17:0] u_prev [29:0];
-reg  [17:0] u_curr [29:0];
+// pio ports for initial values 
+// TODO: make sure to set these to wires when we instantiate the pio ports!!
+reg [17:0] pio_init;
+reg        pio_init_done;
+
+
+// this simulates our pio ports
+reg [5:0] idx; // this tracks where we are in the column
+
+always @(posedge clk) begin
+    if (reset) begin
+        idx <= 6'b0;
+        pio_init <= 18'b0_00000000000100000;
+    end
+    else if (pio_init_done) begin
+
+        if (idx <= 14) begin
+            pio_init <= pio_init + 18'b0_00000000000001000;
+            idx <= idx + 6'd1;
+        end
+        else if (idx == 15) begin
+            pio_init <= pio_init;
+            idx <= idx + 6'd1;
+        end
+        else if (idx > 15 && idx < 30) begin
+            pio_init <= pio_init - 18'b0_00000000000001000;
+            idx <= idx + 6'd1;
+        end
+    end
+end
+
 
 // BOTTOM IDX = 0
 // TOP IDX = 29 (N-1)
 
-generate
-    genvar i;
-    for ( i = 0; i < 30; i = i + 1 ) begin: gen1
-        drum_syn drum_syn_inst (
-            .rho          (18'b0_00010000000000000), // 0.0625
-            .u_top        ((i == 29)  ? (18'd0) : u_curr[i+1]), // if we're at the top, 0 boundary condition
-            .u_bottom     ((i == 0) ? (18'd0) : u_curr[i-1]), // if we're at the bottom, 0 boundary condition
-            .u_left       (18'd0),
-            .u_right      (18'd0),
-            .u_prev       (u_prev[i]),
-            .u_curr       (u_curr[i]),
-            .u_next       (u_next[i])
-        ); 
-    end
-endgenerate
+// generate
+//     genvar i;
+//     for ( i = 0; i < 30; i = i + 1 ) begin: gen1
+//         drum_syn drum_syn_inst (
+//             .rho          (18'b0_00010000000000000), // 0.0625
+//             .u_top        ((i == 29)  ? (18'd0) : u_curr[i+1]), // if we're at the top, 0 boundary condition
+//             .u_bottom     ((i == 0) ? (18'd0) : u_curr[i-1]), // if we're at the bottom, 0 boundary condition
+//             .u_left       (18'd0),
+//             .u_right      (18'd0),
+//             .u_prev       (u_prev[i]),
+//             .u_curr       (u_curr[i]),
+//             .u_next       (u_next[i])
+//         ); 
+//     end
+// endgenerate
+
+// from m10k blocks
+wire [17:0] u_top;
+wire [17:0] u_prev;
+
+// register these and update every iteration
+reg  [17:0] u_bottom;
+reg  [17:0] u_curr;
+
+// our output
+wire [17:0] u_next;
 
 // state machine
 reg [4:0] state;
-reg [5:0] idx; // this tracks where we are in the column
+
+reg write_curr_en; // write to the n m10k block
+reg [17:0] write_curr_data;
+reg [18:0] write_curr_address;
+reg [18:0] read_curr_address;
+
+reg write_prev_en; // write to the n-1 m10k block
+reg [17:0] write_prev_data;
+reg [18:0] write_prev_address;
+reg [18:0] read_prev_address;
+
+reg top_flag;
+
+M10K_1000_8 M10k_u_prev ( 
+    .q             (u_prev), // we always read out u_prev from this m10k
+    .d             (write_prev_data), 
+    .write_address (write_prev_address),
+    .read_address  (read_prev_address),
+    .we            (write_prev_en),
+    .clk           (clk)
+);
+
+M10K_1000_8 M10k_u_curr ( 
+    .q             (u_top),  // we always read out u_top from this m10k
+    .d             (write_curr_data), // we always write u_next to this m10k
+    .write_address (write_curr_address),
+    .read_address  (read_curr_address),
+    .we            (write_curr_en),
+    .clk           (clk)
+);
+
+drum_syn drum_syn_inst (
+            .rho          (18'b0_00010000000000000), // 0.0625
+            .u_top        ((top_flag) ? (18'd0) : u_top), // if we're at the top, 0 boundary condition
+            .u_bottom     (u_bottom), // if we're at the bottom, 0 boundary condition (set in state machine reset)
+            .u_left       (18'd0),
+            .u_right      (18'd0),
+            .u_prev       (u_prev),
+            .u_curr       (u_curr),
+            .u_next       (u_next)
+); 
 
 always @(posedge clk) begin
 
     if (reset) begin
         state <= 5'd0;
         idx   <= 6'd0;
-        u_prev[0] <= 18'b0_00000000000000000;
-        u_curr[0] <= 18'b0_00000000000000000;
+        write_prev_address <= 19'd0;
+        write_curr_address <= 19'd0;
 
+        write_curr_en <= 1'b1;
+        write_prev_en <= 1'b1;
+
+        write_curr_data <= pio_init;
+        write_prev_data <= pio_init;
+
+        // start at bottom 
+        read_prev_address <= 19'd0; 
+        read_curr_address <= 19'd1; // we always read out u_top from here so start at 1 above the bottom
+
+        // set bottom boundary condition 
+        u_bottom <= 18'd0; 
         
-
-        u_prev[15] <= 18'b0_00100000000000000; // 1/8
-        u_curr[15] <= 18'b0_00100000000000000; // 1/8
-
-        u_prev[29] <= 18'b0_00000000000000000;
-        u_curr[29] <= 18'b0_00000000000000000;
+        // set u_curr
+        u_curr <= 18'd0;
+        top_flag <= 1'b0;
     end
     else begin
         case (state)
+            // FIRST TWO STATES ARE OUR INIT STATE MACHINE // 
+            5'd0: begin // start writing initial values
 
-            5'd0: begin // 
+                if (write_prev_address >= 19'd29) begin // once we are done writing all rows start processing
 
+                    write_curr_en <= 1'b0;
+                    write_prev_en <= 1'b0;
+
+                    write_prev_address <= 19'd0;
+                    write_curr_address <= 19'd0;
+
+                    state <= 5'd2;
+                end
+
+                else begin 
+                    write_prev_address <= write_prev_address + 19'd1;
+                    write_curr_address <= write_curr_address + 19'd1;
+
+                    write_prev_en <= 1'b1;
+                    write_curr_en <= 1'b1;
+
+                    write_prev_data <= pio_init;
+                    write_curr_data <= pio_init;
+
+                    pio_init_done <= 1'b1; 
+                    state <= 5'd1;
+                end
+                
             end
-            5'd1:
-            5'd2:
-            5'd3:
+            5'd1: begin // second init state
+                pio_init_done <= 1'b0;
+                state <= 5'd0;
+            end
 
+
+            // START OF OUR REGULAR STATE MACHINE // 
+
+            5'd2: begin // wait one cycle for read data to come back
+                write_curr_en   <= 1'b0;
+                write_prev_en   <= 1'b0;
+                state <= 5'd3;
+            end
+
+            5'd3: begin
+                // write u_next to n m10k (update NEXT timestep for this node)
+                write_curr_en   <= 1'b1;
+
+                // write u_curr to n-1 m10k (update PREV timestep for this node)
+                write_prev_en   <= 1'b1;  
+                write_prev_data <= u_curr;
+
+                // move up one node (in CURR timestep)
+                u_curr          <= u_top;  
+
+                if (read_prev_address == 19'd28) begin
+                    top_flag <= 1'b1;
+                end
+                
+                // state transition
+                if (read_prev_address >= 19'd29) begin // we've finished the column
+
+                    // restart from the bottom!
+
+                    u_bottom <= 18'd0; // set bottom boundary condition
+
+                    top_flag <= 1'b0;
+
+                    write_curr_address <= 19'd0;
+                    write_prev_address <= 19'd0;
+
+                    read_prev_address <= 19'd0;
+                    read_curr_address <= 19'd1;
+
+                    state <= 5'd2;
+                end
+                else begin
+                    // move up one node (in CURR timestep)
+                    u_bottom        <= u_curr; 
+
+                    write_prev_address <= write_prev_address + 19'd1;
+                    write_curr_address <= write_curr_address + 19'd1;
+
+                    read_prev_address <= read_prev_address + 19'd1;
+                    read_curr_address <= read_curr_address + 19'd1;
+
+                    state <= 5'd2;
+                end
+                
+            end
 
         endcase
     end
@@ -86,23 +255,6 @@ always @(posedge clk) begin
 end
 
 
-M10K_1000_8 M10k_u_prev ( 
-    .q             (), 
-    .d             (u_curr),
-    .write_address (),
-    .read_address  (),
-    .we            (),
-    .clk           (clk)
-);
-
-M10K_1000_8 M10k_u_curr ( 
-    .q             (), 
-    .d             (u_next),
-    .write_address (),
-    .read_address  (),
-    .we            (),
-    .clk           (clk)
-);
 
 
 endmodule
@@ -141,7 +293,6 @@ module drum_syn (
         .a   (rho),
         .b   (int_sum)
     );
-
 
 endmodule
 
@@ -208,7 +359,6 @@ module M10K_1000_8(
     input we, clk
 );
 	 // force M10K ram style
-	 // 307200 words of 8 bits
     reg [17:0] mem [68266:0]  /* synthesis ramstyle = "no_rw_check, M10K" */;
 	// reg [7:0] mem [153600:0]; // 2 solvers
 	// reg [7:0] mem [76800:0]; // 4 solvers
